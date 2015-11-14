d47258a1e7cb188c62d1da7ac69e1b0a7a4dcfa9
Alice Stang <alice.stang@st.ovgu.de>
2015-11-14 12:00:58 +0100

    Erstes Layout - Buttons

--- Left2eat/Left2eat.Droid/Resources/Resource.designer.cs
+++ Left2eat/Left2eat.Droid/Resources/Resource.designer.cs
@@ -60,8 +60,44 @@ namespace Left2eat.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int button2 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int button3 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button4 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int button5 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int button6 = 2131034123;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int editText2 = 2131034117;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout2 = 2131034115;
+			
+			// aapt resource value: 0x7f05000c
+			public const int myButton = 2131034124;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView2 = 2131034116;
 			
 			static Id()
 			{
@@ -77,7 +113,19 @@ namespace Left2eat.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Edit = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Foodsharing = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Hfcontent = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Homefridge = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Main = 2130903044;
 			
 			static Layout()
 			{
@@ -92,11 +140,59 @@ namespace Left2eat.Droid
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040007
+			public const int all = 2130968583;
+			
 			// aapt resource value: 0x7f040001
 			public const int app_name = 2130968577;
 			
+			// aapt resource value: 0x7f040011
+			public const int category = 2130968593;
+			
+			// aapt resource value: 0x7f040004
+			public const int cookbook = 2130968580;
+			
+			// aapt resource value: 0x7f04000e
+			public const int drinks = 2130968590;
+			
+			// aapt resource value: 0x7f04000a
+			public const int edit = 2130968586;
+			
+			// aapt resource value: 0x7f04000b
+			public const int everything = 2130968587;
+			
+			// aapt resource value: 0x7f040003
+			public const int fcontent = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int foodsharing = 2130968578;
+			
+			// aapt resource value: 0x7f04000c
+			public const int fruit = 2130968588;
+			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int homefridge = 2130968576;
+			
+			// aapt resource value: 0x7f040010
+			public const int label = 2130968592;
+			
+			// aapt resource value: 0x7f040009
+			public const int link = 2130968585;
+			
+			// aapt resource value: 0x7f04000f
+			public const int meat = 2130968591;
+			
+			// aapt resource value: 0x7f040008
+			public const int nearyou = 2130968584;
+			
+			// aapt resource value: 0x7f040005
+			public const int random = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int reversecookbook = 2130968582;
+			
+			// aapt resource value: 0x7f04000d
+			public const int vegetable = 2130968589;
 			
 			static String()
 			{
